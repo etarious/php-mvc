f98a2ec4d705b452e9ada6637496de77ce283d05
etarious <ojameruayeetaoghene@gmail.com>
2023-12-16 02:28:54 +0100

    My first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+spl_autoload_register(function (string $class_name) {
+
+    // var_dump(str_replace("\\", "/", $class_name));
+    
+    require "src/" . str_replace("\\", "/", $class_name) . ".php";
+
+});
+
+$router = new Framework\Router;
+
+$router->add("/", ["controller" => "home", "action" => "index"]);
+$router->add("/home/index", ["controller" => "home", "action" => "index"]);
+$router->add("/products/index", ["controller" => "products", "action" => "index"]);
+$router->add("/products/show", ["controller" => "products", "action" => "show"]);
+
+$params = $router->match($path);
+
+if ($params === false) {
+    
+    exit("No route found");
+
+}
+
+$action = $params["action"];
+$controller = "App\Controllers\\" . ucwords($params["controller"]);
+
+$controller_object = new $controller;
+
+$controller_object->$action();
---
+++ src/App/Controllers/Products.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Product;
+
+class Products
+{
+    public function index()
+    {
+
+        $model = new Product;
+
+        $products = $model->getData();
+
+        require "views/product_index.php";
+    }
+
+    public function show()
+    {
+        require "views/product_show.php";
+    }
+}
